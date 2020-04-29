7ba3916ecca9867fb91f2dd2658a5ef16c41bdb5
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-04-29 15:51:45 +0900

    ADD pprint & ssl

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pprint
+import ssl
 
-html = urlopen("http://www.naver.com")
+context = ssl._create_unverified_context()
+pp=pprint.PrettyPrinter(indent=4)
+html = urlopen("http://www.naver.com", context = context)
 bsObject = BeautifulSoup(html, "html.parser")
 
-print(bsObject)
+for meta in bsObject.head.find_all('meta'):
+    print(meta.get('content'))
