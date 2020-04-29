8a17d2cda957eb3915ccc72c7d3076cc9d23ce01
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-04-29 22:27:06 +0900

    Change code to look more comfortable

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import ssl
 
 context = ssl._create_unverified_context()
 
-html = urlopen("http://www.myashley.co.kr/Menu/SaladBar/Menu08.aspx", context=context)
+html = urlopen(
+    "http://www.myashley.co.kr/Menu/SaladBar/Menu08.aspx", context=context)
 bsObject = BeautifulSoup(html, "html.parser")
-parseImg = bsObject.body.find_all('img')
+parseImg = bsObject.find("div", class_="season_sp").find_all("img")
 
+cnt = 1
 for menu in parseImg:
-     print(menu.get('alt'))
+    print(cnt," : ", menu.get('alt'))
+    cnt+=1
