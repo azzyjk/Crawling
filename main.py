b7970f37b29dacf02b92e018e66ffe0a8f25624e
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-04-29 19:04:53 +0900

    crawling ashley menu img alt for find 'mint choco'

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import pprint
 import ssl
 
 context = ssl._create_unverified_context()
-pp=pprint.PrettyPrinter(indent=4)
-html = urlopen("http://www.naver.com", context = context)
-bsObject = BeautifulSoup(html, "html.parser")
 
-for meta in bsObject.head.find_all('meta'):
-    print(meta.get('content'))
+html = urlopen("http://www.myashley.co.kr/Menu/SaladBar/Menu08.aspx", context=context)
+bsObject = BeautifulSoup(html, "html.parser")
+parseImg = bsObject.body.find_all('img')
+
+for menu in parseImg:
+     print(menu.get('alt'))
