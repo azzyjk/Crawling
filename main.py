0147ae84ce78a7f1970758584826d7344496c791
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-04-30 14:13:18 +0900

    Finish crawling ashley w class menu

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 from func import *
 
-menu()
-choose = int(input("Select number : "))
-
-# context = makeCont()
-# menu = urlopen(wSite, context=context)
-# bsObject = BeautifulSoup(menu, "html.parser")
-# parseImg = bsObject.find("div", class_="season_sp").find_all("img")
-# print(parseImg)
-
-# parseImg = menuFind(wSite)
-# showMenu(parseImg)
-
-if(choose == 1):
-    html, classType = menuType("classic")
-    parseImg = menuFind(html, classType)
-elif(choose == 2):
-    html, classType = menuType("w")
-    parseImg = menuFind(html, classType)
+print("This is ashely's W class menu")
+html, classType = menuType("w")
+parseImg = menuFind(html, classType)
 
 showMenu(parseImg)
