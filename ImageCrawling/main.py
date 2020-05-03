69146031e24c7f5cd311906e7d00409a174f8f28
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-05-03 14:25:44 +0900

    Test image crawling using screenshot function

--- ImageCrawling/main.py
+++ ImageCrawling/main.py
@@ -1,39 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import json
-import os
-import urllib.request
 
-searchTerm = 'person'
-header = {'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36"}
+searchTerm = '라이언'
 url = "https://www.google.com/search?q="+searchTerm+"&source=lnms&tbm=isch"
+browser = webdriver.Chrome('/usr/local/bin/Chromedriver')
 
-browser = webdriver.Chrome('/usr/local/bin/chromedriver')
+
 browser.get(url)
+temp = browser.find_elements_by_class_name("rg_i")
 
+picture = temp[0]
 
-
-# a = borwser.find
-# for i in range(200):
-#     browser.execute_script("window.scrollBy(0,10000)")
-
-# for idx, el in enumerate(browser.find_elements_by_class_name("rg_ic")):
-#     el.screenshot(str(idx) + ".png")
-
-
-# cnt = 0
-# succCnt = 0
-
-# print(os.path)
-
-# if not os.path.exists(searchTerm):
-#     os.mkdir(searchTerm)
-
-# for _ in range(500):
-#     browser.execute_script("window.scrollBy(0,10000)")
-
-# for x in browser.find_element_by_xpath('//div[contains(@class, "rg_meta")]'):
-#     cnt += 1
-#     print("Total Count : ", cnt)
-#     print("Successful Count : ", succCnt)
-#     print("URL : ", json.loads(x.get_attribute('innerHTML'))["ou"])
+picture.screenshot("test.png")
