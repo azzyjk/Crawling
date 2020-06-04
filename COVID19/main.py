fc91bd5b023653066c2dbc8b22ec42738212f398
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-06-05 01:13:43 +0900

    Save parse data at list

--- COVID19/main.py
+++ COVID19/main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
+import time
 
 site = "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=12&ncvContSeq=&contSeq=&board_id=&gubun="
 
@@ -8,6 +9,9 @@ page = requests.get(site)
 bsObject = BeautifulSoup(page.text, "html.parser")
 parseLoc = bsObject.find_all('td')
 
-parseLoc = str(parseLoc).replace("<td>","").replace("</td>","\n")
-
-print(parseLoc)
+parseLoc = str(parseLoc).replace("<td>","").replace("</td>","\n").replace("<br/>","\n")
+lists = []
+lists = parseLoc.split('\n')
+for tst in lists:
+    time.sleep(1)
+    print(tst)
