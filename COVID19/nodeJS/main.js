1ef15eca0598a22c3aaa7320cee629acfa9e018c
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-06-14 02:09:19 +0900

    Crawling site

---
+++ COVID19/nodeJS/main.js
@@ -0,0 +1,19 @@
+const request = require("request");
+const cheerio = require("cheerio");
+const iconv = require("iconv-lite");
+
+var url =
+  "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=12&ncvContSeq=&contSeq=&board_id=&gubun=";
+
+request(
+  {
+    uri: url,
+    method: "GET",
+    encoding: "binary",
+  },
+  function (err, resp, body) {
+    body = iconv.decode(body, "utf-8");
+    const $ = cheerio.load(body);
+    console.log($("div").text());
+  }
+);
