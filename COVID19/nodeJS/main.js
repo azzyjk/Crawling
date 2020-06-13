a5a667620e732539ef87e468828cf58596234698
azzyjk <56161518+azzyjk@users.noreply.github.com>
2020-06-14 02:32:54 +0900

    Change module 'request' to 'axios'

--- COVID19/nodeJS/main.js
+++ COVID19/nodeJS/main.js
@@ -1,19 +1,27 @@
-const request = require("request");
+const axios = require("axios");
 const cheerio = require("cheerio");
 const iconv = require("iconv-lite");
 
-var url =
-  "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=12&ncvContSeq=&contSeq=&board_id=&gubun=";
+var url = {
+  url:
+    "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=12&ncvContSeq=&contSeq=&board_id=&gubun=",
+  method: "GET",
+  responseType: "arraybuffer",
+  responseEncoding: "binary",
+};
 
-request(
-  {
-    uri: url,
-    method: "GET",
-    encoding: "binary",
-  },
-  function (err, resp, body) {
-    body = iconv.decode(body, "utf-8");
-    const $ = cheerio.load(body);
-    console.log($("div.box_line2 table.midsize.big tbody").text());
+const getHtml = async () => {
+  try {
+    const response = await axios(url);
+    const html = iconv.decode(response.data, "utf-8").toString();
+    return html;
+  } catch (error) {
+    console.error(error);
   }
-);
+};
+
+getHtml().then((html) => {
+  const $ = cheerio.load(html);
+  const $text = $("div.box_line2 table.midsize.big tbody").text();
+  console.log($text);
+});
